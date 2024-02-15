f39e17427127208effde40c96ca2d031dfaf57a6
Jinaymehta2512 <jinayyyyy2003@gmail.com>
2024-02-15 18:50:34 +0530

    done vercel setting

--- moviereviews/settings.py
+++ moviereviews/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-&#=jk73442gnw^ddnmrvh!#!f3qr!kw8_hf*g(mzmy+vf^om%+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app' , '.now.sh']
 
 
 # Application definition
@@ -134,3 +134,4 @@ STATICFILES_DIRS = [
 ]
 
 LOGIN_URL = 'loginaccount'
+
